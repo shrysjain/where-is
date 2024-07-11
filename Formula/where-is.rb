9fcab801af2b61138eab85c1c70987c4779cd7f6
Shreyas Jain <shrys.jain@gmail.com>
2024-07-11 19:24:07 -0400

    typo

--- Formula/where-is.rb
+++ Formula/where-is.rb
@@ -1,8 +1,8 @@
 class Whereis < Formula
   desc "A command-line tool to easily find files and directories"
   homepage "https://github.com/shrysjain/where-is"
-  url "https://github.com/shrysjain/whereis/archive/v1.0.tar.gz"
-  sha256 "YOUR_SHA256_HASH"
+  url "https://github.com/shrysjain/where-is/archive/v1.0.tar.gz"
+  sha256 ""
   license "MIT"
 
   def install
